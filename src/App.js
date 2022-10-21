2b5ea83b993aebae371e427b1468ae78890cc9e4
nazeerbshaik <shaiknazeer1995@gmail.com>
2022-10-21 22:46:49 +0530

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+import React,{ useState} from 'react'
 import './App.css';
 
 function App() {
+  const [input,setInput] = useState('');
+  const [result,setResult] = useState(0);
+  const handler = e =>{
+      setInput(e.target.value);
+  } 
+  const f1 = () => setInput('');
+  const f2 = () => setResult('');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+        <div className='result'>
+            <input type="text"  value={input} name="input" onChange={handler}/>
+            <br />
+            <h4>Result :{result}</h4>
+            <button className='res-btn' onClick = {()=> setResult(eval(input))}>Result</button>
+        </div>
+        <div className='input'>
+            <button onClick={() => setInput(input+'1')}>1</button>
+            <button onClick={() => setInput(input+'2')}>2</button>
+            <button onClick={() => setInput(input+'3')}>3</button>
+            <button onClick={() => setInput(input+'4')}>4</button>
+            <button onClick={() => setInput(input+'5')}>5</button><br/>
+
+            <button onClick={() => setInput(input+'6')}>6</button>
+            <button onClick={() => setInput(input+'7')}>7</button>
+            <button onClick={() => setInput(input+'8')}>8</button>
+            <button onClick={() => setInput(input+'9')}>9</button>
+            <button onClick={() => setInput(input+'0')}>0</button><br/>
+
+            <button onClick={() => setInput(input+'+')}>+</button>
+            <button onClick={() => setInput(input+'-')}>-</button>
+            <button onClick={() => setInput(input+'*')}>*</button>
+            <button onClick={() => setInput(input+'/')}>/</button>
+            <button className='clr' onClick={() => {f1();f2();}}>clr</button>
+        </div>  
     </div>
+
   );
 }
 
